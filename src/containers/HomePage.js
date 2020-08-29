a32162080b28a8090bb9000c8840c2991638febd
AnaNeri <ana.neri@nery.name>
2020-08-29 22:31:41 +0100

    small fixes

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,31 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import Header from '../components/Header';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import key from '../key.json';
 
 
 function HomePageContent() {
-  const useFetch = () => {
-    const [data, setData] = useState(null);
-
-    useEffect(() => {
-      async function fetchData() {
-        const url = "https://api.openweathermap.org/data/2.5/weather?id=8010448&appid=" + key.value;
-        const response= await fetch(url);
-        const data = await response.json();
-        const item =  data.weather[0].description;
-        setData(item.toString().toLowerCase());
-      }
-      fetchData()
-    }, []);
-    return data;
-  };
-
-  const data = useFetch();
-  
+    
   return (
     <div >
        <Header />
@@ -36,7 +18,6 @@ function HomePageContent() {
             <Row>
             <Col md={11}>
             <p>Hello! Welcome to a proud ravenclaw's blogs, feel free to check my latest ramblings.</p>
-            { data && <p>Since it's {data} outside, why not learn something new? :) </p> }
 
         <br/>
 
