205544e2af53c30a4a579942b408f1b1dde4706f
AnaNeri <ana.neri@nery.name>
2021-06-01 19:52:39 +0100

    Update AboutMe.js

--- src/containers/AboutMe.js
+++ src/containers/AboutMe.js
@@ -15,17 +15,29 @@ function AboutMeContent() {
 			<Col md={11}>
        <h2>About me</h2>
        	<br/>
-	       <p> I am a PhD student at the <a href="https://mapi.map.edu.pt/" target="_blank" rel="noopener noreferrer">MAPi</a> doctoral program with a project on Quantum Computing in the Computer Science Department. 
-	       Additionally, I am also associated with the <a href="https://haslab.uminho.pt/" target="_blank" rel="noopener noreferrer">HASLab </a>  
+	       <p> 
+			   I am a PhD student at the 
+			   <a href="https://mapi.map.edu.pt/" target="_blank" rel="noopener noreferrer">MAPi</a>
+			   doctoral program with a project on Quantum Computing. 
+			   I've got selected for a 
+			   <a href="https://quantumportugalinitiative.inl.int/" target="_blank" rel="noopener noreferrer">Quantum Portugal Initiative FCT-INL scholarship</a>. 
+			   I'm affiliated with 
+			   <a href="https://inl.int/" target="_blank" rel="noopener noreferrer">INL</a>, 
+			   <a href="https://haslab.uminho.pt/" target="_blank" rel="noopener noreferrer">HASLab </a>
+			   <a href= "https://inesctec.pt/en" target="_blank" rel="noopener noreferrer"> INESC TEC</a>, and 
+			   <a href="https://cidma.ua.pt/" target="_blank" rel="noopener noreferrer"> CIDMA</a>. 
+			</p>
 
-	       <a href= "https://inesctec.pt/en" target="_blank" rel="noopener noreferrer"> INESC TEC</a> as a External student. 
-	       In the HASLab, I also have the pleasure of working with the <a href="https://arca.di.uminho.pt/" target="_blank" rel="noopener noreferrer">ARCA team</a>.</p>
+			<p>
+				I teach at the University of Minho, and I'm a member of the 
+				<a href="https://arca.di.uminho.pt/" target="_blank" rel="noopener noreferrer">ARCA team</a> 
+				and 
+				<a href="http://www.quantalab.org" target="_blank" rel="noopener noreferrer">QuantaLab</a>
+			</p>
 
-	       <p>Furthermore, I am also affiliated with <a href="http://www.quantalab.org" target="_blank" rel="noopener noreferrer">QuantaLab</a>.</p>
-
-	       <p>My current work focuses on the quantum computing field. </p>
-
-	       <p>The goal of one of my recent projects was to apply quantum software (like <a href="https://qiskit.org/" target="_blank" rel="noopener noreferrer">QISKit</a>) to make a practical analysis of the impact of quantum computing in a particular problem given by <a href="https://www.bial.com/en/" target="_blank" rel="noopener noreferrer">Bial</a>.</p>
+	        <p>
+				My current work focuses on the quantum computing field. 
+			</p>
     	</Col>
     	</Row>
     	</Col>
